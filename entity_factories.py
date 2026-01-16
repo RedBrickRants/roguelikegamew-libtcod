1e72ed39bdd61c31518c28478d42cfc8cf22b09d
RedBrickRants <davidwarodney@gmail.com>
2026-01-16 01:27:11 -0500

    added area of effect spell and confusion spell

--- entity_factories.py
+++ entity_factories.py
@@ -21,3 +21,16 @@ lightning_scroll = Item(
     name="Lightning Scroll",
     consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
 )
+confusion_scroll = Item(
+    glyph="~",
+    colour=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+
+fireball_scroll = Item(
+    glyph="~",
+    colour=(255, 0, 0),
+    name="Fireball Scroll",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
+)
