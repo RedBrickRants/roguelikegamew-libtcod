ddc2c6fad10259a2a2c5d77a9d60141eee82b15f
RedBrickRants <davidwarodney@gmail.com>
2026-01-17 00:11:49 -0500

    added player levels and leveling up

--- entity.py
+++ entity.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:
     from components.consumable import Consumable
     from components.fighter import Fighter
     from components.inventory import Inventory
+    from components.level import Level
     from game_map import GameMap
 
 T = TypeVar("T", bound="Entity")
@@ -81,6 +82,7 @@ class Actor(Entity):
             ai_cls: Type[BaseAI],
             fighter: Fighter,
             inventory: Inventory,
+            level: Level,
     ):
         super().__init__(
             x=x,
@@ -97,6 +99,9 @@ class Actor(Entity):
         self.fighter.parent = self
         self.inventory = inventory
         self.inventory.parent = self
+        self.level = level
+        self.level.parent = self
+        
     @property
     def is_alive(self)->bool:
         return bool(self.ai)
--- entity_factories.py
+++ entity_factories.py
@@ -2,12 +2,13 @@ from components.ai import HostileEnemy
 from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
+from components.level import Level
 from entity import Actor, Item
 
-player = Actor(glyph="@", colour=(255, 255, 255), name="Player",ai_cls=HostileEnemy,  fighter=Fighter(hp=30, defence=2, power=5),inventory=Inventory(capacity=26),)
+player = Actor(glyph="@", colour=(255, 255, 255), name="Player",ai_cls=HostileEnemy,  fighter=Fighter(hp=30, defence=2, power=5),inventory=Inventory(capacity=26),level=Level(level_up_base=200),)
 
-orc = Actor(glyph="o", colour=(63, 127, 63), name="Orc",ai_cls=HostileEnemy, fighter=Fighter(hp=10, defence=0, power=3),inventory=Inventory(capacity=0),)
-troll = Actor(glyph="T", colour=(0, 127, 0), name="Troll",ai_cls=HostileEnemy, fighter=Fighter(hp=16, defence=1, power=4),inventory=Inventory(capacity=0),)
+orc = Actor(glyph="o", colour=(63, 127, 63), name="Orc",ai_cls=HostileEnemy, fighter=Fighter(hp=10, defence=0, power=3),inventory=Inventory(capacity=0),level=Level(xp_given=35),)
+troll = Actor(glyph="T", colour=(0, 127, 0), name="Troll",ai_cls=HostileEnemy, fighter=Fighter(hp=16, defence=1, power=4),inventory=Inventory(capacity=0),level=Level(xp_given=100),)
 
 health_potion = Item(
     glyph="!",
